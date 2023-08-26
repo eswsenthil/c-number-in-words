c4fe1d97e3851aca887f2e96ec0c1efca69521ac
eswsenthil <103065446+eswsenthil@users.noreply.github.com>
2023-08-26 15:10:42 +0300

    Initial commit

---
+++ number-in-words/number-in-words/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  number-in-words
+//
+//  Created by Tester on 26.8.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
