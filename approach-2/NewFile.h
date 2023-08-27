c02302d44b560434d9612b2467ddedc90b9935c8
eswsenthil <103065446+eswsenthil@users.noreply.github.com>
2023-08-27 15:35:56 +0300

    Add approach-2

---
+++ approach-2/NewFile.h
@@ -0,0 +1,15 @@
+//
+//  NewFile.h
+//  test-senthil
+//
+//  Created by Tester on 27.8.2023.
+//
+
+#ifndef NewFile_h
+#define NewFile_h
+
+#include "ft.h"
+
+void    ft_new_print(unsigned long long n, t_list *tab);
+
+#endif /* NewFile_h */
